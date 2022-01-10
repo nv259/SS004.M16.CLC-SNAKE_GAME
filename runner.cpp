2dddf959793c4399a9a7e0f1993b7144707c289f
UIT-21520436 <96657816+hatrontai@users.noreply.github.com>
2022-01-10 22:26:48 +0700

    update restart game and game level

--- runner.cpp
+++ runner.cpp
@@ -13,8 +13,11 @@ int player_score;
 bool cheat = false;
 bool End_game = false;
 int count_phase = 1;
-// Ð?c Nhân
-void gotoxy(int column, int line){
+bool dead_by_multiuniverse = false;
+int time_left_for_portal_to_disappear;
+// ï¿½?c Nhï¿½n
+void gotoxy(int column, int line)
+{
 	COORD coord{};
 	coord.X = column;
 	coord.Y = line;
@@ -52,6 +55,8 @@ public:
             snake[0].pre_y = snake[0].y;
 		}
 
+		bool temp = false;
+
         switch (direct) {
         case 'a':
             snake[0].x--;
@@ -74,8 +79,15 @@ public:
 			snake[i].pre_y = snake[i].y;
 			snake[i].x = snake[i - 1].pre_x;
 			snake[i].y = snake[i - 1].pre_y;
+			if (abs(snake[i-1].x - snake[i].x) + abs(snake[i-1].y - snake[i].y) > 1)
+            {
+                temp = true;
+            }
 		}
 		first_step = false;
+		is_going_through_portal = temp;
+		if (time_left_for_portal_to_disappear <= 0 && is_going_through_portal)
+			dead_by_multiuniverse = true;
 	}
 
 	void init() {
@@ -86,6 +98,15 @@ public:
 		snake[1].y = 10;
 		snake[2].x = 16;
 		snake[2].y = 10;
+
+        /*
+		// init the snake with length = 20
+		for (int i = 3; i < 20; i++)
+		{
+			snake[i].x = 14 + i;
+			snake[i].y = 10;
+		}
+        */
 	}
 
 	void draw() {
@@ -103,9 +124,9 @@ public:
 	int x, y;
     int type;
     // Type 1: Th?c an thu?ng
-    // Type 2: x5 di?m nh?n vào
+    // Type 2: x5 di?m nh?n vï¿½o
     // Type 3: x2 t?c d? ch?y :)))))))))))
-    // type 4: -5*base_score (n?u âm thì set l?i là 0)
+    // type 4: -5*base_score (n?u ï¿½m thï¿½ set l?i lï¿½ 0)
 	void make_food() {
 		/* std::random_device seed;
 		std::mt19937 gen(seed());
@@ -161,7 +182,6 @@ public:
 };
 
 bool portal_is_opening;
-int time_left_for_portal_to_disappear;
 class PORTAL {
 public:
     int x, y;
@@ -220,7 +240,15 @@ public:
 	}
 };
 
-bool Game_over(SNAKE s, std::string& reason){
+bool Game_over(SNAKE s, std::string& reason)
+{
+	/* portal cut the snake */
+	if (dead_by_multiuniverse)
+	{
+		reason = "Oh no! Your tail is teleported to another universe, you're shocked to dead!";
+		return true;
+	}
+
 	/* snake hit the wall */
 	if (s.snake[0].x > 30 || s.snake[0].x < 1 || s.snake[0].y > 20 || s.snake[0].y < 1)
 	{
@@ -274,7 +302,60 @@ PORTAL portal1, portal2;
 void Game_level(){
     std::string level;
     int _level;
-    std::cout<<"Vui long chon cap do tu 1 --> 5 \nPs: cap do la so nguyen lon hon 0 va be hon 6 \n";
+    std::cout<<"Hay chon cap do cho minh tu 1 --> 5 \
+      \nPs: cap do la so nguyen lon hon 0 va be hon 6 \n";
+    std::cin>>level;
+    if (level == "862006")
+    {
+        //std::cout << "You are a loser";
+        End_game = true;
+    }
+    else
+    {
+        if (level == "21520378")
+            _level = 6;
+        else
+        {
+            while(1)
+            {
+              if(level.size()==1)
+                if(level[0]=='1' || level[0]=='2' || level[0]=='3' || level[0]=='4' || level[0]=='5')
+                    break;
+               std::cout<<"Vui long nhap so tu 1-->5 \n";
+               std::cin>>level;
+            }
+            _level=level[0]-'0';
+            base_score=5*_level;
+            game_speed-=40*_level;
+            system("cls");
+            std::string hello = "------CHAO MUNG BAN DEN VOI SNAKE GAME-----<3\nSu dung 'a' 'w' 'd' 's' de di chuyen\nchon 'r' hay 'R' de choi lai tu dau\nchon 'x' de dung tam thoi";
+            for(int i = 0; i<hello.size(); i++)
+            {
+                std::cout<<hello[i];
+                Sleep(50);
+            }
+            Sleep(2000);
+        }
+        if (_level == 6)
+        {
+            std::string cheating = "You are cheating.(#_<-)";
+            for (int i = 0; i < cheating.size(); i++)
+            {
+                std::cout << cheating[i];
+                Sleep(50);
+            }
+            Sleep(500);
+            cheat = true;
+            base_score = 150;
+            game_speed = 150;
+        }
+    }
+
+}
+void Game_level_1(){
+    std::string level;
+    int _level;
+    std::cout<<"Vui long chon lai cap do tu 1 --> 5 \n";
     std::cin>>level;
     if (level == "862006")
     {
@@ -324,7 +405,7 @@ char curr_direct(SNAKE s) {
 }
 
 void try_make_portal() {
-    if (rand() % 1 == 0)
+    if (rand() % 10 == 0)
     {
         portal1.init(snake, fruit, portal2);
         portal2.init(snake, fruit, portal1);
@@ -365,18 +446,20 @@ void Restart()
 }
 int main()
 {
+    int k=0;
     REPLAY:{
     Restart();
-    Game_level();
+    if(!k) { Game_level(); k=1;}
+    else Game_level_1();
 
     if (End_game)
     {
         char X = (char)254;
-        std::cout << "X   X  XX  X  X    X  XX  XXX    X    X    XX  XXX XXX XX \n";
-        std::cout << " X X  X  X X  X   X X X X X     X X   X   X  X X   X   X X \n";
-        std::cout << "  X   X  X X  X   XXX XX  XXX   XXX   X   X  X XXX XXX XX \n";
-        std::cout << "  X   X  X X  X   X X X X X     X X   X   X  X   X X   X X \n";
-        std::cout << "  X    XX   XX    X X X X XXX   X X   XXX  XX  XXX XXX X X \n";
+        std::cout << "X   X  XX  X  X    X  XX  XXX    X    X    XX  XXX XXX XX\n";
+        std::cout << " X X  X  X X  X   X X X X X     X X   X   X  X X   X   X X\n";
+        std::cout << "  X   X  X X  X   XXX XX  XXX   XXX   X   X  X XXX XXX XX\n";
+        std::cout << "  X   X  X X  X   X X X X X     X X   X   X  X   X X   X X\n";
+        std::cout << "  X    XX   XX    X X X X XXX   X X   XXX  XX  XXX XXX X  X\n";
         return 0;
     }
     // Nh?p sai cheatcode
